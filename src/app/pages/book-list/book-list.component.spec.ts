34af096bc8afa8dc1b1eeb876af7560a8fd82c3e
”Heorhii <hradinarpay@gmail.com>
2023-03-24 18:12:29 +0300

    add test

--- src/app/pages/book-list/book-list.component.spec.ts
+++ src/app/pages/book-list/book-list.component.spec.ts
@@ -2,24 +2,54 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookListComponent } from './book-list.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FilterService } from '../../services/filter.service';
+import { Filters } from '../../interfaces/filters.interface';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('BookListComponent', () => {
   let component: BookListComponent;
   let fixture: ComponentFixture<BookListComponent>;
+  let filterServiceSpy: jasmine.SpyObj<FilterService>;
 
   beforeEach(async () => {
+    const spyService = jasmine.createSpyObj('FilterService', ['getFilteredBooks']);
+
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      declarations: [ BookListComponent ]
+      declarations: [BookListComponent],
+      providers: [
+        {provide: FilterService, useValue: spyService},
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(BookListComponent);
     component = fixture.componentInstance;
+    filterServiceSpy = TestBed.inject(FilterService) as jasmine.SpyObj<FilterService>;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('changeFilteredBooks() should', () => {
+    it('call filtersService with updated filters', () => {
+      filterServiceSpy.getFilteredBooks.and.returnValue([]);
+      const filters: Filters = {
+        search: '',
+        genre: '',
+        languages: [],
+        authors: [],
+        pageStart: 0,
+        pageEnd: 0
+      }
+      component.books = [];
+
+      component.changeFilteredBooks(filters);
+
+      expect(filterServiceSpy.getFilteredBooks).toHaveBeenCalledOnceWith(filters, []);
+    });
   });
 });
